70e40855e190eea8b2484a16ad1ce2ea657177f3
Roman Smolyakov <123468411+Roman9456@users.noreply.github.com>
2023-05-30 20:51:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import os
 import random
 import telebot 
-from telebot import types 
+from telebot import types
 
-
-
-# Путь к папке с картинками
-IMAGE_FOLDER = 'Введите путь до директории'
-
+# Path to the image folder
+IMAGE_FOLDER = 'Enter the path to the directory'
 
 TOKEN_FILE = 'token.txt'
 
@@ -17,22 +14,22 @@ def get_token():
         token = token_line.strip().split('=')[1]
         return token
 
-# Функция старта 
+# Start function
 bot = telebot.TeleBot(get_token())
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, 'Привет, путник: выбери папку', reply_markup=get_folder_keyboard())
+    bot.send_message(message.chat.id, "Hello traveler, choose a folder", reply_markup=get_folder_keyboard())
 
-# Получение сообщения от пользователя
+# Receive user messages
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    if message.text.startswith('Папка: '):
-        folder_name = message.text.replace('Папка: ', '')
+    if message.text.startswith('Folder: '):
+        folder_name = message.text.replace('Folder: ', '')
         send_random_image(message.chat.id, folder_name)
     else:
-        bot.send_message(message.chat.id, 'Неверный выбор. Попробуйте еще раз.', reply_markup=get_folder_keyboard())
+        bot.send_message(message.chat.id, 'Invalid choice. Please try again.', reply_markup=get_folder_keyboard())
 
-# Отправка случайной картинки из выбранной папки
+# Send a random image from the selected folder
 def send_random_image(chat_id, folder_name):
     folder_path = os.path.join(IMAGE_FOLDER, folder_name)
     image_files = os.listdir(folder_path)
@@ -42,15 +39,16 @@ def send_random_image(chat_id, folder_name):
         with open(image_path, 'rb') as f:
             bot.send_photo(chat_id, f)
     else:
-        bot.send_message(chat_id, f'В папке {folder_name} нет изображений.')
+        bot.send_message(chat_id, f'There are no images in the folder: {folder_name}.')
 
-# Получение клавиатуры с кнопками выбора папок
+# Get the keyboard with folder selection buttons
 def get_folder_keyboard():
     keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
     folders = os.listdir(IMAGE_FOLDER)
     for folder in folders:
-        button = types.KeyboardButton(f'Папка: {folder}')
+        button = types.KeyboardButton(f'Folder: {folder}')
         keyboard.add(button)
     return keyboard
 
 bot.polling(none_stop=True, interval=0)
+
